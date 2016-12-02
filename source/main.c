35f2488eb7648949d9834caa8c33f54435a4f932
Brandon Cornel <bcc4679@rit.edu>
2016-12-02 15:34:09 -0500

    WE DID IT

--- source/main.c
+++ source/main.c
@@ -1,6 +1,7 @@
 #include "gpio.h"
 #include "system_timer.h"
 #include "uart.h"
+#include "morse.h"
 
 void blink_once()
 {
@@ -32,82 +33,18 @@ int main()
 {
     // Implement Lab 4 as described in the lab manual
     int counter = 0;
-    const int bufferSize = 80;
+    const int bufferSize = 200;
     char buffer [bufferSize];
-    uint32_t w;
-    uint32_t x;
-    uint32_t y;
-    uint32_t z;
-    uint32_t f;
-    uint32_t g;
-	uint32_t a;
-    uint32_t b;
-    uint32_t c;
-    uint32_t d;
-    uint32_t R1;
-    uint32_t R2;
-	
-
 	while(1){
-		put_string("Enter 1 or 2:");
+		put_string("Enter the sentence you want in morse code:");
 		put_string("\r\n");
-		char input = get_char();
-		if (input == '1'){
-			put_string("w x y z f g");
-			put_string("\r\n");
+		size_t charsGot = get_string(buffer, bufferSize);
+		for(size_t i = 0;i < charsGot;i++){
+			char ch = buffer[i];
+			translate(ch);
+		}
+		//translate(input);
 	
-			for(w=0; w<2;w++){
-				for (x=0; x<2; x++){
-					for (y=0;y<2; y++){
-						for(z=0;z<2;z++){
-							put_char(w|0x30);
-							put_char(' ');
-							put_char(x|0x30);
-							put_char(' ');
-							put_char(y|0x30);
-							put_char(' ');
-							put_char(z|0x30);
-							put_char(' ');
-							put_char(((!w&&!x&&!y&&!z)||(!w&&!x&&y&&!z)||(!w&&x&&!y&&!z)||(!w&&x&&y&&z)||(!w&&x&&y&&!z)||(w&&x&&!y&&z)||(w&&x&&y&&z)||(w&&x&&y&&!z)|(w&&!x&&!y&&z))|0x30);\
-							put_char(' ');
-							put_char(((!w&&!z)||(w&&!y&&z)||(x&&y))|0x30);
-							put_string("\r\n");
-						}
-						
-					}
-				}
-		
-		
-			}
-		}
-		else if(input == '2'){
-			
-			put_string("a b c d R1 R2");
-			put_string("\r\n");
-			for(a=0; a<2;a++){
-				for (b=0; b<2; b++){
-					for (c=0;c<2; c++){
-						for(d=0;d<2;d++){
-							put_char(a|0x30);
-							put_char(' ');
-							put_char(b|0x30);
-							put_char(' ');
-							put_char(c|0x30);
-							put_char(' ');
-							put_char(d|0x30);
-							put_char(' ');
-							put_char(((!b&!d)|(b&c))|0x30);
-							put_char(' ');
-							put_char(((!b&!d)|(c&!d))|0x30);
-							put_string("\r\n");
-						}
-						
-					}
-				}
-			}
-		}
-		//else{
-		//	put_string("Please enter 1 or 2\r\n");
 		
         
     
---
+++ source/morse.c
@@ -0,0 +1,100 @@
+#include "gpio.h"
+#include "uart.h"
+#include "morse.h"
+
+extern const char *morse_translate(const char c) {
+  lower_string(c);
+  switch(c) {
+  case 'a':
+    return ".-";
+  case 'b':
+    return "-...";
+  case 'c':
+    return "-.-.";
+  case 'd':
+    return "-..";
+  case 'e':
+    return ".";
+  case 'f':
+    return "..-.";
+  case 'g':
+    return "--.";
+  case 'h':
+    return "....";
+  case 'i':
+    return "..";
+  case 'j': 
+    return ".---";
+  case 'k':
+    return "-.-";
+  case 'l':
+    return ".-..";
+  case 'm':
+    return "--";
+  case 'n':
+    return "-.";
+  case 'o':
+    return "---";
+  case 'p':
+    return ".--.";
+  case 'q':
+    return "--.-";
+  case 'r':
+    return ".-.";
+  case 's':
+    return "...";
+  case 't':
+    return "-";
+  case 'u':
+    return "..-";
+  case 'v':
+    return "...-";
+  case 'w':
+    return ".--";
+  case 'x':
+    return "-..-";
+  case 'y':
+    return "-.--";
+  case 'z':
+    return "--..";
+  case '0':
+    return "-----";
+  case '1':
+    return ".----";
+  case '2':
+    return "..---";
+  case '3':
+    return "...--";
+  case '4':
+    return "....-";
+  case '5':
+    return ".....";
+  case '6':
+    return "-....";
+  case '7':
+    return "--...";
+  case '8':
+    return "---..";
+  case '9':
+    return "----.";
+  case ' ':
+	return "-----";
+  default:
+    return " ";
+  }
+
+}
+
+extern void lower_string(char chr){
+	if(chr>='A' && chr<='Z'){
+		chr = chr + 32;
+	}
+}
+
+
+
+extern void translate(const char str){
+	put_string(	morse_translate(str));
+
+	
+}
